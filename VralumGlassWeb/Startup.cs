8a44b692613b83653bcfbabb870c2fc597fce1a3
anton <anton@vayosoft.com>
2019-03-10 13:28:55 +0200

    ...

--- VralumGlassWeb/Startup.cs
+++ VralumGlassWeb/Startup.cs
@@ -29,12 +29,12 @@ namespace VralumGlassWeb
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.Configure<CookiePolicyOptions>(options =>
-			{
-				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
-				options.CheckConsentNeeded = context => true;
-				options.MinimumSameSitePolicy = SameSiteMode.None;
-			});
+			//services.Configure<CookiePolicyOptions>(options =>
+			//{
+			//	// This lambda determines whether user consent for non-essential cookies is needed for a given request.
+			//	options.CheckConsentNeeded = context => true;
+			//	options.MinimumSameSitePolicy = SameSiteMode.None;
+			//});
 
 			services.AddDbContext<ApplicationDbContext>(options =>
 				options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
@@ -66,7 +66,7 @@ namespace VralumGlassWeb
 
 			//app.UseHttpsRedirection();
 			app.UseStaticFiles();
-			app.UseCookiePolicy();
+			//app.UseCookiePolicy();
 
 			app.UseAuthentication();
 
