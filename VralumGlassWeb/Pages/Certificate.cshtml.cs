e6c84085955dbb495f0e14e2869924667df51a97
dravgust <dravgust@hotmail.com>
2019-03-25 22:09:46 +0200

    init

--- VralumGlassWeb/Pages/Certificate.cshtml.cs
+++ VralumGlassWeb/Pages/Certificate.cshtml.cs
@@ -4,8 +4,11 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using VralumGlassWeb.Data;
 using VralumGlassWeb.Data.Models;
 
 namespace VralumGlassWeb.Pages
@@ -13,21 +16,32 @@ namespace VralumGlassWeb.Pages
     public class CertificateModel : PageModel
     {
         private readonly IHostingEnvironment _hostingEnvironment;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ILogger<SupportModel> _logger;
 
         [BindProperty]
         public Customer Customer { get; set; }
 
-        public CertificateModel(IHostingEnvironment hostingEnvironment)
+        public CertificateModel(IHostingEnvironment hostingEnvironment, SignInManager<IdentityUser> signInManager, ILogger<SupportModel> logger)
         {
             _hostingEnvironment = hostingEnvironment;
+            _signInManager = signInManager;
+            _logger = logger;
         }
 
-        public void OnGet(string id)
+        public IActionResult OnGet(string id)
         {
+            if (_signInManager.IsSignedIn(User))
+            {
+                return Redirect("~/Management/Defect?id=" + id);
+            }
+
             Customer = new Customer
             {
                 CustomerId = id
             };
+
+            return Page();
         }
 
         public async Task<IActionResult> OnPostExport()
