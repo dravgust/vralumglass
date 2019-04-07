ccf39c5041e70fa2fdadffc5f4b6f08a200c2ced
dravgust <dravgust@hotmail.com>
2019-04-07 18:05:59 +0300

    init

---
+++ Varalumglass.Test/Plank.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Varalumglass.Test
+{
+    /// <summary>
+    /// A generic 'Plank'
+    /// </summary>
+    public class Plank
+    {
+        public Plank(float length)
+        {
+            OriginalLength = length;
+        }
+
+        public float FreeLength => OriginalLength - Cuts.Sum();
+
+        public float OriginalLength;
+
+        public readonly List<float> Cuts = new List<float>();
+
+        public void Cut(float cutLength)
+        {
+            Cuts.Add(cutLength);
+        }
+    }
+}
