db5f7bb41f157c9e147fc85ee3fe8623fa025c96
dravgust <dravgust@hotmail.com>
2019-04-13 17:16:00 +0300

    add qr-coder reader

---
+++ VralumGlassWeb/Areas/Management/Pages/QrCodeReader.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using VralumGlassWeb.Data.Models;
+using VralumGlassWeb.Pages;
+
+namespace VralumGlassWeb.Areas.Management.Pages
+{
+	[Authorize]
+    public class QrCodeReaderModel : PageModel
+    {
+        private readonly ILogger<SupportModel> _logger;
+
+		public QrCodeReaderModel(ILogger<SupportModel> logger)
+        {
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public ManagementDefect Defect { get; set; }
+
+		public IActionResult OnGet()
+        {
+			return Page();
+		}
+	}
+}
