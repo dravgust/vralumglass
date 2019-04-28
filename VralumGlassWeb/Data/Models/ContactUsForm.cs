98672ede0ef117e7131f41e97932a894dba4908f
anton <anton@vayosoft.com>
2019-04-28 10:53:16 +0300

    init

---
+++ VralumGlassWeb/Data/Models/ContactUsForm.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VralumGlassWeb.Data.Models
+{
+    public class ContactUsForm
+    {
+        [Required, Display(Name = "Name"), StringLength(20)]
+        public string Name { get; set; }
+
+        [Required, Display(Name = "Phone"), StringLength(20)]
+        public string Phone { get; set; }
+
+        [Required, Display(Name = "Subject"), StringLength(128)]
+        public string Subject { get; set; }
+
+        [Required, Display(Name = "Message"), StringLength(500)]
+        public string Message { get; set; }
+    }
+}
--- VralumGlassWeb/Pages/Index.cshtml.cs
+++ VralumGlassWeb/Pages/Index.cshtml.cs
@@ -3,23 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 using Vralumglass.Core;
+using VralumGlassWeb.Data.Models;
 
 namespace VralumGlassWeb.Pages
 {
 	public class IndexModel : PageModel
 	{
 	    private readonly ILogger<IndexModel> _logger;
-	    private readonly IStringLocalizer<IndexModel> _resources;
+        private readonly IEmailSender _emailSender;
+        private readonly IStringLocalizer<IndexModel> _resources;
 
-        public IndexModel(IStringLocalizer<IndexModel> resources, ILogger<IndexModel> logger)
+        [BindProperty]
+        public ContactUsForm ContactUsForm { get; set; }
+
+        public IndexModel(IStringLocalizer<IndexModel> resources, IEmailSender emailSender, ILogger<IndexModel> logger)
 	    {
             _resources = resources;
+            _emailSender = emailSender;
 	        _logger = logger;
 	    }
 
@@ -28,6 +35,21 @@ namespace VralumGlassWeb.Pages
             TempData["_welcome"] = $"res - {_resources["_welcome"]}";
             _logger.LogInformation("INIT");
 		}
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var email = "dravgust@hotmail.com";
+            await _emailSender.SendEmailAsync(email, ContactUsForm.Subject, $"{ContactUsForm.Name}\r\n{ContactUsForm.Phone}\r\n{ContactUsForm.Message}");
+
+            TempData["alerts"] = new List<string> { "Message sent successfully." };
+
+            return Redirect("~/");
+        }
 
         public IActionResult OnPostSetLanguage(string culture, string returnUrl)
         {
